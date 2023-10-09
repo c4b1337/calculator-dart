0cd4213eed87a8530890c1a7106f2927bb113b6a
user <user@users-MacBook-Pro.local>
2023-10-09 16:22:37 +0300

    add algorithm, change menu, add comment and minor fix

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -27,7 +27,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 reverse: true,
                 child: Container(
                   alignment: Alignment.bottomRight,
-                  padding: const EdgeInsets.all(18),
+                  padding: const EdgeInsets.all(16),
                   child: Text(
                     "$number1$operand$number2".isEmpty
                         ? "0"
@@ -42,7 +42,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               ),
             ),
 
-            //buttons
+            // buttons
             Wrap(
               children: Btn.buttonValues
                   .map(
@@ -50,12 +50,12 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                       width: value == Btn.n0
                           ? screenSize.width / 2
                           : (screenSize.width / 4),
-                      height: screenSize.width / 4,
+                      height: screenSize.width / 5,
                       child: buildButton(value),
                     ),
                   )
                   .toList(),
-            ),
+            )
           ],
         ),
       ),
@@ -66,20 +66,23 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Material(
-        clipBehavior: Clip.hardEdge,
         color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
         shape: OutlineInputBorder(
           borderSide: const BorderSide(
             color: Colors.white24,
           ),
-          borderRadius: BorderRadius.circular(100.0),
+          borderRadius: BorderRadius.circular(100),
         ),
         child: InkWell(
           onTap: () => onBtnTap(value),
           child: Center(
             child: Text(
               value,
-              style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
             ),
           ),
         ),
@@ -87,39 +90,169 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     );
   }
 
-//On button tap.
-
-void onBtnTap (String value){
-
-  if (value!=Btn.dot&&int.tryParse(value)==null){
-    if(operand.isNotEmpty&&number2.isNotEmpty){
-
+  // ########
+  void onBtnTap(String value) {
+    if (value == Btn.del) {
+      delete();
+      return;
     }
-    operand = value;
-  }else if (number1.isEmpty || operand.isEmpty){
-    if (value==Btn.dot && number1.contains(Btn.dot)) return;
-    if (value==Btn.dot && number1.isEmpty || number1==Btn.n0) {
-      value = "0.";
+
+    if (value == Btn.clr) {
+      clearAll();
+      return;
     }
-    number1 += value;
-  }else if (number2.isEmpty || operand.isNotEmpty){
-    if (value==Btn.dot && number2.contains(Btn.dot)) return;
-    if (value==Btn.dot && number2.isEmpty || number2==Btn.n0) {
-      value = "0.";
+
+    if (value == Btn.per) {
+      convertToPercentage();
+      return;
     }
-    number2 += value;
-  
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendValue(value);
   }
 
-  setState(() {});
-}
+  // ##############
+  // calculates the result
+  void calculate() {
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
 
-  Color? getBtnColor(value) {
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      default:
+    }
+
+    setState(() {
+      number1 = result.toStringAsPrecision(3);
+
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  // ##############
+  // converts output to %
+  void convertToPercentage() {
+    // ex: 434+324
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      // calculate before conversion
+      calculate();
+    }
+
+    if (operand.isNotEmpty) {
+      // cannot be converted
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 = "${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  // ##############
+  // clears all output
+  void clearAll() {
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  // ##############
+  // delete one from the end
+  void delete() {
+    if (number2.isNotEmpty) {
+      // 12323 => 1232
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
+
+  // #############
+  // appends value to the end
+  void appendValue(String value) {
+    // number1 opernad number2
+    // 234       +      5343
+
+    // if is operand and not "."
+    if (value != Btn.dot && int.tryParse(value) == null) {
+      // operand pressed
+      if (operand.isNotEmpty && number2.isNotEmpty) {
+        //TODO: calculate the equation before assigning new operand
+        calculate();
+      }
+      operand = value;
+    }
+    // assign value to number1 variable
+    else if (number1.isEmpty || operand.isEmpty) {
+      // check if value is "." | ex: number1 = "1.2"
+      if (value == Btn.dot && number1.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number1.isEmpty || number1 == Btn.n0)) {
+        // ex: number1 = "" | "0"
+        value = "0.";
+      }
+      number1 += value;
+    }
+    // assign value to number2 variable
+    else if (number2.isEmpty || operand.isNotEmpty) {
+      // check if value is "." | ex: number1 = "1.2"
+      if (value == Btn.dot && number2.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number2.isEmpty || number2 == Btn.n0)) {
+        // number1 = "" | "0"
+        value = "0.";
+      }
+      number2 += value;
+    }
+
+    setState(() {});
+  }
+
+  // ########
+  Color getBtnColor(value) {
     return [Btn.del, Btn.clr].contains(value)
         ? Colors.blueGrey
-        : [Btn.subtract, Btn.add, Btn.multiply, Btn.divide, Btn.per]
-                .contains(value)
+        : [
+            Btn.per,
+            Btn.multiply,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate,
+          ].contains(value)
             ? Colors.orange
-            : Colors.grey[900];
+            : Colors.black87;
   }
 }
